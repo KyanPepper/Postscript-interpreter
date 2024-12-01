342028649a31d26daa7a0d05be8f2f1bee88bdb0
kyanpepper <kyankks1@gmail.com>
2024-11-30 17:22:24 -0800

    Finshed interpreter

---
+++ main.py
@@ -0,0 +1,238 @@
+class PostScriptInterpreter:
+    def __init__(self, use_lexical_scoping=False):
+        # Initialize the operand stack and the dictionary stack
+        self.stack = []
+        self.dict_stack = [{}]
+        self.use_lexical_scoping = use_lexical_scoping
+
+    def execute(self, command):
+        # Execute a list of commands or a single command
+        if isinstance(command, list):
+            for cmd in command:
+                self.execute(cmd)
+        elif isinstance(command, str):
+            if command in self.commands():
+                self.commands()[command]()
+            elif command.isdigit() or (command[0] == '-' and command[1:].isdigit()):
+                self.stack.append(int(command))
+            else:
+                self.stack.append(command)
+        else:
+            self.stack.append(command)
+
+    def exch(self):
+        # Exchange the top two elements on the stack
+        self.stack[-1], self.stack[-2] = self.stack[-2], self.stack[-1]
+
+    def pop(self):
+        # Pop the top element from the stack
+        self.stack.pop()
+
+    def copy(self):
+        # Copy the top n elements on the stack
+        n = self.stack.pop()
+        self.stack.extend(self.stack[-n:])
+
+    def dup(self):
+        # Duplicate the top element on the stack
+        self.stack.append(self.stack[-1])
+
+    def clear(self):
+        # Clear the stack
+        self.stack.clear()
+
+    def count(self):
+        # Push the number of elements on the stack
+        self.stack.append(len(self.stack))
+
+    def add(self):
+        # Add the top two elements on the stack
+        self.stack.append(self.stack.pop() + self.stack.pop())
+
+    def sub(self):
+        # Subtract the top element from the second top element on the stack
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a - b)
+
+    def mul(self):
+        # Multiply the top two elements on the stack
+        self.stack.append(self.stack.pop() * self.stack.pop())
+
+    def div(self):
+        # Divide the second top element by the top element on the stack
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a / b)
+
+    def idiv(self):
+        # Integer divide the second top element by the top element on the stack
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a // b)
+
+    def mod(self):
+        # Modulo the second top element by the top element on the stack
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a % b)
+
+    def abs(self):
+        # Push the absolute value of the top element on the stack
+        self.stack.append(abs(self.stack.pop()))
+
+    def neg(self):
+        # Negate the top element on the stack
+        self.stack.append(-self.stack.pop())
+
+    def ceiling(self):
+        # Push the ceiling of the top element on the stack
+        self.stack.append(int(self.stack.pop() + 0.999999))
+
+    def floor(self):
+        # Push the floor of the top element on the stack
+        self.stack.append(int(self.stack.pop()))
+
+    def round(self):
+        # Push the rounded value of the top element on the stack
+        self.stack.append(round(self.stack.pop()))
+
+    def sqrt(self):
+        # Push the square root of the top element on the stack
+        self.stack.append(self.stack.pop() ** 0.5)
+
+    def dict(self):
+        # Push an empty dictionary onto the stack
+        self.stack.append({})
+
+    def length(self):
+        # Push the length of the top element on the stack
+        self.stack.append(len(self.stack.pop()))
+
+    def maxlength(self):
+        # Push the length of the top element on the stack (same as length)
+        self.stack.append(len(self.stack.pop()))
+
+    def begin(self):
+        # Begin a new dictionary scope
+        self.dict_stack.append(self.stack.pop())
+
+    def end(self):
+        # End the current dictionary scope
+        self.dict_stack.pop()
+
+    def def_(self):
+        # Define a new key-value pair in the current dictionary scope
+        value = self.stack.pop()
+        key = self.stack.pop()
+        if self.use_lexical_scoping:
+            self.dict_stack[-1][key] = value
+        else:
+            for d in reversed(self.dict_stack):
+                if key in d:
+                    d[key] = value
+                    return
+            self.dict_stack[-1][key] = value
+
+    def eq(self):
+        # Push True if the top two elements are equal, else False
+        self.stack.append(self.stack.pop() == self.stack.pop())
+
+    def ne(self):
+        # Push True if the top two elements are not equal, else False
+        self.stack.append(self.stack.pop() != self.stack.pop())
+
+    def ge(self):
+        # Push True if the second top element is greater than or equal to the top element, else False
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a >= b)
+
+    def gt(self):
+        # Push True if the second top element is greater than the top element, else False
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a > b)
+
+    def le(self):
+        # Push True if the second top element is less than or equal to the top element, else False
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a <= b)
+
+    def lt(self):
+        # Push True if the second top element is less than the top element, else False
+        b, a = self.stack.pop(), self.stack.pop()
+        self.stack.append(a < b)
+
+    def and_(self):
+        # Push the logical AND of the top two elements on the stack
+        self.stack.append(self.stack.pop() & self.stack.pop())
+
+    def or_(self):
+        # Push the logical OR of the top two elements on the stack
+        self.stack.append(self.stack.pop() | self.stack.pop())
+
+    def not_(self):
+        # Push the logical NOT of the top element on the stack
+        self.stack.append(not self.stack.pop())
+
+    def true(self):
+        # Push True onto the stack
+        self.stack.append(True)
+
+    def false(self):
+        # Push False onto the stack
+        self.stack.append(False)
+
+    def print_(self):
+        # Print the top element on the stack
+        print(self.stack.pop())
+
+    def eq_(self):
+        # Print the top element on the stack (same as print_)
+        print(self.stack.pop())
+
+    def commands(self):
+        # Return a dictionary of command names to methods
+        return {
+            "exch": self.exch,
+            "pop": self.pop,
+            "copy": self.copy,
+            "dup": self.dup,
+            "clear": self.clear,
+            "count": self.count,
+            "add": self.add,
+            "sub": self.sub,
+            "mul": self.mul,
+            "div": self.div,
+            "idiv": self.idiv,
+            "mod": self.mod,
+            "abs": self.abs,
+            "neg": self.neg,
+            "ceiling": self.ceiling,
+            "floor": self.floor,
+            "round": self.round,
+            "sqrt": self.sqrt,
+            "dict": self.dict,
+            "length": self.length,
+            "maxlength": self.maxlength,
+            "begin": self.begin,
+            "end": self.end,
+            "def": self.def_,
+            "eq": self.eq,
+            "ne": self.ne,
+            "ge": self.ge,
+            "gt": self.gt,
+            "le": self.le,
+            "lt": self.lt,
+            "and": self.and_,
+            "or": self.or_,
+            "not": self.not_,
+            "true": self.true,
+            "false": self.false,
+            "print": self.print_,
+            "=": self.eq_,
+        }
+
+if __name__ == "__main__":
+    # Create an instance of the interpreter with dynamic scoping and execute a series of commands
+    interpreter_dynamic = PostScriptInterpreter(use_lexical_scoping=False)
+    interpreter_dynamic.execute(["/x", "10", "def", "x", "print"])  # Should print 10
+
+    # Create an instance of the interpreter with lexical scoping and execute a series of commands
+    interpreter_lexical = PostScriptInterpreter(use_lexical_scoping=True)
+    interpreter_lexical.execute(["/x", "10", "def", "x", "print"])  # Should print 10
---
+++ tests.py
@@ -0,0 +1,144 @@
+import pytest
+from main import PostScriptInterpreter  
+
+@pytest.fixture
+def interpreter():
+    return PostScriptInterpreter()
+
+@pytest.fixture
+def interpreter_lexical():
+    return PostScriptInterpreter(use_lexical_scoping=True)
+
+def test_push_number(interpreter):
+    interpreter.execute("42")
+    assert interpreter.stack == [42]
+
+def test_push_negative_number(interpreter):
+    interpreter.execute("-42")
+    assert interpreter.stack == [-42]
+
+def test_exch(interpreter):
+    interpreter.execute(["1", "2", "exch"])
+    assert interpreter.stack == [2, 1]
+
+def test_pop(interpreter):
+    interpreter.execute(["1", "2", "pop"])
+    assert interpreter.stack == [1]
+
+def test_copy(interpreter):
+    interpreter.execute(["1", "2", "3", "2", "copy"])
+    assert interpreter.stack == [1, 2, 3, 2, 3]
+
+def test_dup(interpreter):
+    interpreter.execute(["1", "dup"])
+    assert interpreter.stack == [1, 1]
+
+def test_clear(interpreter):
+    interpreter.execute(["1", "2", "3", "clear"])
+    assert interpreter.stack == []
+
+def test_count(interpreter):
+    interpreter.execute(["1", "2", "3", "count"])
+    assert interpreter.stack == [1, 2, 3, 3]
+
+def test_add(interpreter):
+    interpreter.execute(["1", "2", "add"])
+    assert interpreter.stack == [3]
+
+def test_sub(interpreter):
+    interpreter.execute(["3", "1", "sub"])
+    assert interpreter.stack == [2]
+
+def test_mul(interpreter):
+    interpreter.execute(["3", "2", "mul"])
+    assert interpreter.stack == [6]
+
+def test_div(interpreter):
+    interpreter.execute(["6", "3", "div"])
+    assert interpreter.stack == [2.0]
+
+def test_idiv(interpreter):
+    interpreter.execute(["7", "3", "idiv"])
+    assert interpreter.stack == [2]
+
+def test_mod(interpreter):
+    interpreter.execute(["7", "3", "mod"])
+    assert interpreter.stack == [1]
+
+def test_abs(interpreter):
+    interpreter.execute(["-7", "abs"])
+    assert interpreter.stack == [7]
+
+def test_neg(interpreter):
+    interpreter.execute(["7", "neg"])
+    assert interpreter.stack == [-7]
+
+def test_ceiling(interpreter):
+    interpreter.execute(["7.1", "ceiling"])
+    assert interpreter.stack == [8]
+
+def test_floor(interpreter):
+    interpreter.execute(["7.9", "floor"])
+    assert interpreter.stack == [7]
+
+def test_round(interpreter):
+    interpreter.execute(["7.6", "round"])
+    assert interpreter.stack == [8]
+
+def test_sqrt(interpreter):
+    interpreter.execute(["9", "sqrt"])
+    assert interpreter.stack == [3.0]
+
+def test_dict(interpreter):
+    interpreter.execute(["dict"])
+    assert interpreter.stack == [{}]
+
+def test_length(interpreter):
+    interpreter.execute(["[1, 2, 3]", "length"])
+    assert interpreter.stack == [3]
+
+def test_def_and_lookup_dynamic():
+    interpreter = PostScriptInterpreter(use_lexical_scoping=False)
+    interpreter.execute(["/x", "10", "def", "x"])
+    assert interpreter.stack == [10]
+
+def test_def_and_lookup_lexical(interpreter_lexical):
+    interpreter_lexical.execute(["/x", "10", "def", "x"])
+    assert interpreter_lexical.stack == [10]
+
+def test_eq(interpreter):
+    interpreter.execute(["10", "10", "eq"])
+    assert interpreter.stack == [True]
+
+def test_ne(interpreter):
+    interpreter.execute(["10", "20", "ne"])
+    assert interpreter.stack == [True]
+
+def test_gt(interpreter):
+    interpreter.execute(["20", "10", "gt"])
+    assert interpreter.stack == [True]
+
+def test_lt(interpreter):
+    interpreter.execute(["10", "20", "lt"])
+    assert interpreter.stack == [True]
+
+def test_and(interpreter):
+    interpreter.execute(["True", "False", "and"])
+    assert interpreter.stack == [False]
+
+def test_or(interpreter):
+    interpreter.execute(["True", "False", "or"])
+    assert interpreter.stack == [True]
+
+def test_not(interpreter):
+    interpreter.execute(["False", "not"])
+    assert interpreter.stack == [True]
+
+def test_true_false(interpreter):
+    interpreter.execute(["true", "false"])
+    assert interpreter.stack == [True, False]
+
+def test_print(interpreter, capsys):
+    interpreter.execute(["10", "print"])
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "10"
